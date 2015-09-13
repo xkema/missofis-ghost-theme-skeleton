c46f057f8c7f9dc6f93879d83d0351d7ff5887e2
Kemal Yılmaz <kemalxyz@gmail.com>
2015-09-13 23:08:35 +0300

    Update .gitignore file Rename custom.css to skeleton-overrides.css (removed in next commit) Clean up default.hbs base Update screen.css to serve as standalone css file (remove skeleton-overides.css, .. etc) Remove inline cg-hages in skeleton.css (use overrides for all customizations) Remove main.js scripts (older menu collape/expand controls) Add Zepo.min.js and touch.js Zepto extension Update post.hbs, loop.hbs, navigation.hbs Add fontawesome csn to webfonts.hbs partial

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Skeleton Theme for Ghost Platform
 TODO
 ----
 
+* remove skeleton overrides
+* json ld data author page, contact page, ..
+* it'a personal blog not millions of daily visit magazine
+* add an end marker to last paragraphs in posts (a square, bullet, ..?)
+* swipe events (zepto touch?)
+* publish forked and updated/customized version of theme
 * Robots.txt
 * Humans.txt
 * h5bp
@@ -14,3 +20,4 @@ TODO
 * http://blog.teamtreehouse.com/getting-started-with-handlebars-js
 * responsive video?
 * `<!-- remove@production --><script src="//localhost:35729/livereload.js"></script><!-- endremove@production -->`
+* toc module?
---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+// 
+Zepto( function( $ ) {
+
+	/*
+
+	// 
+	$( document ).on( 'swipe', function( e ) {
+
+		alert( 'swiped' );
+
+	} );
+
+	// navigation toggle button controls
+	$( '.nav-toggle' ).on( 'click', function( e ) {
+
+		e.stopImmediatePropagation();
+		$this = $( this );
+		$navSite = $( '.nav-site' );
+		$navSite.toggle();
+
+	} );
+
+	// hide navigation on document click
+	$( document ).on( 'click', function( e ) {
+
+		if( 'none' !== $( '.nav-toggle' ).css( 'display' ).trim() ) {
+			$navSite = $( '.nav-site' );
+			$navSite.hide();
+		}
+
+	} );
+
+	// window resized (bugfix for orientation change navigation ...)
+	$( window ).resize( function( e ) {
+
+		if( 'none' === $( '.nav-toggle' ).css( 'display' ).trim() ) {
+			$navSite = $( '.nav-site' );
+			$navSite.show();
+		}
+
+
+	} );
+
+	*/
+
+
+} );
